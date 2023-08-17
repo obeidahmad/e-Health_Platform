d540316b7641f51170ac89da990625ae5d1a2d2c
Ahmad Obeid <obeid.ahmad2001@outlook.com>
2023-08-17 20:34:26 +0300

    Complete auth server

--- fyp-be/Auth/main.py
+++ fyp-be/Auth/main.py
@@ -1,5 +1,6 @@
 import firebase_admin
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from firebase_admin import credentials, auth, firestore
 from firebase_admin.auth import EmailAlreadyExistsError, UserNotFoundError, InvalidIdTokenError
 from starlette.middleware.cors import CORSMiddleware
@@ -102,8 +103,6 @@ async def signup(user_credentials: DoctorSignup):
         return {"token": custom_token}
 
 
-# @app.post()
-
 @app.get('/validate_token')
 async def get_user(token: str):
     try:
@@ -114,3 +113,149 @@ async def get_user(token: str):
     except InvalidIdTokenError as e:
         print(e)
         raise HTTPException(status_code=400, detail="Bad token")
+
+
+class JWTBearerService(HTTPBearer):
+    def __init__(self, role: str = ""):
+        self.role = role
+        super(JWTBearerService, self).__init__()
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearerService, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            try:
+                user = auth.verify_id_token(credentials.credentials)
+            except UserNotFoundError:
+                raise HTTPException(status_code=404, detail="This user does not exist")
+            except InvalidIdTokenError as e:
+                raise HTTPException(status_code=403, detail=e)
+            if (self.role == "employee" and user["role"] not in ["nurse", "doctor", "admin"]) or (
+                    self.role == "patient" and user["role"] != "patient") or (
+                    self.role == "doctor" and user["role"] != "doctor") or (
+                    self.role == "nurse" and user["role"] != "nurse") or (
+                    self.role == "nurse_patient" and user["role"] not in ["nurse", "patient"]):
+                raise HTTPException(status_code=403, detail="Not Authorized")
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+
+@app.get("/user", dependencies=[Depends(JWTBearerService())])
+def auth_user_get():
+    return
+
+
+@app.post("/user", dependencies=[Depends(JWTBearerService())])
+def auth_user_post():
+    return
+
+
+@app.delete("/user", dependencies=[Depends(JWTBearerService())])
+def auth_user_delete():
+    return
+
+
+@app.put("/user", dependencies=[Depends(JWTBearerService())])
+def auth_user_put():
+    return
+
+
+@app.get("/employee", dependencies=[Depends(JWTBearerService(role="employee"))])
+def auth_employee_get():
+    return
+
+
+@app.post("/employee", dependencies=[Depends(JWTBearerService(role="employee"))])
+def auth_employee_post():
+    return
+
+
+@app.delete("/employee", dependencies=[Depends(JWTBearerService(role="employee"))])
+def auth_employee_delete():
+    return
+
+
+@app.put("/employee", dependencies=[Depends(JWTBearerService(role="employee"))])
+def auth_employee_put():
+    return
+
+
+@app.get("/patient", dependencies=[Depends(JWTBearerService(role="patient"))])
+def auth_patient_get():
+    return
+
+
+@app.post("/patient", dependencies=[Depends(JWTBearerService(role="patient"))])
+def auth_patient_post():
+    return
+
+
+@app.delete("/patient", dependencies=[Depends(JWTBearerService(role="patient"))])
+def auth_patient_delete():
+    return
+
+
+@app.put("/patient", dependencies=[Depends(JWTBearerService(role="patient"))])
+def auth_patient_put():
+    return
+
+
+@app.get("/doctor", dependencies=[Depends(JWTBearerService(role="doctor"))])
+def auth_doctor_get():
+    return
+
+
+@app.post("/doctor", dependencies=[Depends(JWTBearerService(role="doctor"))])
+def auth_doctor_post():
+    return
+
+
+@app.delete("/doctor", dependencies=[Depends(JWTBearerService(role="doctor"))])
+def auth_doctor_delete():
+    return
+
+
+@app.put("/doctor", dependencies=[Depends(JWTBearerService(role="doctor"))])
+def auth_doctor_put():
+    return
+
+
+@app.get("/nurse_patient", dependencies=[Depends(JWTBearerService(role="nurse_patient"))])
+def auth_nurse_patient_get():
+    return
+
+
+@app.post("/nurse_patient", dependencies=[Depends(JWTBearerService(role="nurse_patient"))])
+def auth_nurse_patient_post():
+    return
+
+
+@app.delete("/nurse_patient", dependencies=[Depends(JWTBearerService(role="nurse_patient"))])
+def auth_nurse_patient_delete():
+    return
+
+
+@app.put("/nurse_patient", dependencies=[Depends(JWTBearerService(role="nurse_patient"))])
+def auth_nurse_patient_put():
+    return
+
+
+@app.get("/nurse", dependencies=[Depends(JWTBearerService(role="nurse"))])
+def auth_nurse_get():
+    return
+
+
+@app.post("/nurse", dependencies=[Depends(JWTBearerService(role="nurse"))])
+def auth_nurse_post():
+    return
+
+
+@app.delete("/nurse", dependencies=[Depends(JWTBearerService(role="nurse"))])
+def auth_nurse_delete():
+    return
+
+
+@app.put("/nurse", dependencies=[Depends(JWTBearerService(role="nurse"))])
+def auth_nurse_put():
+    return
