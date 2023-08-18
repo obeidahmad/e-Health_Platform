241ecb667d57c2d72d0440abfe0437e6ffd6e170
Afrah <afrah.hassan@inmind.ai>
2023-08-18 18:14:17 +0300

    seeder fixes

--- fyp-be/dev-utils/src/main.py
+++ fyp-be/dev-utils/src/main.py
@@ -1,6 +1,7 @@
 try:
     from setup import create_schemas
-    from seeders import seed_med_forms, seed_med_class, seed_medicines, create_roles, seed_mock_users
+    from seeders import seed_med_forms , seed_med_class, seed_medicines , create_roles, seed_mock_users
+    from seeders import seed_purchase_status
 except Exception as e:
     print(e)
     pass
